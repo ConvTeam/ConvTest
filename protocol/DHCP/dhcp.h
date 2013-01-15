12f1117ae0a4d3e16705f212008005f44017fe44
Jeong GW <jeonggw@wiznet.co.kr>
2013-01-15 20:27:34 +0900

    aa
    aa

--- protocol/DHCP/dhcp.h
+++ protocol/DHCP/dhcp.h
@@ -10,7 +10,7 @@
 #define DHCP_STATE_REQUESTING	3
 #define DHCP_STATE_IP_CHECK		4
 #define DHCP_STATE_BOUND		5
-
+#define DHCP_STATE_BOUND		6
 
 int8 dhcp_init(SOCKET sock, pFunc ip_update, pFunc ip_conflict, uint8 *my_mac);
 int8 dhcp_get_state(void);
